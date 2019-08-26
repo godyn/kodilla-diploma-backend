8908d692e259f0553cafa2de07d8126ad7fc2bae
godyn <godyn.ola@gmail.com>
2019-08-26 12:58:30 +0200

    Initial commit

---
+++ src/main/java/com/kodilla/kodilla/diplomaBackend/KodillaDiplomaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.kodilla.diplomaBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaDiplomaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaDiplomaBackendApplication.class, args);
+    }
+
+}
